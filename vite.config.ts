1b38402450d1cf0fc1a77f4a41da1785bd0a1952
Bryan Miller <bmilcs@yahoo.com>
2023-03-27 16:01:34 -0400

    Add sass & vite-plugin-eslint packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
 
   // "@" becomes the default path for src
   resolve: {
