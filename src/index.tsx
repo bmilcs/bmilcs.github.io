8055b350c733c323739b206378639198c9f4ecc6
Bryan Miller <bmilcs@yahoo.com>
2023-03-27 16:05:11 -0400

    Add css reset, font imports, font & color vars

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/App';
-import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
