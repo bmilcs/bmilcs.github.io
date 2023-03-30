02d0c1132fda7fe6438d81ba16ee3ac5cd9d63c9
Bryan Miller <bmilcs@yahoo.com>
2023-03-30 15:39:00 -0400

    Add global module types: jpg, png

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
+declare module '*.jpg' {
+  const path: string;
+  export default path;
+}
+
+declare module '*.png' {
+  const path: string;
+  export default path;
+}
+
 // / <reference types="vite/client" />
