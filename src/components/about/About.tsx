2d28806c87e3a3412903ff0b7446f487ce2fe923
Bryan Miller <bmilcs@yahoo.com>
2023-04-09 15:43:11 -0400

    Add skills to & style about section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,43 +1,100 @@
+import Babel from '../../assets/Babel';
+import CSS from '../../assets/CSS';
+import ESLint from '../../assets/ESLint';
+import Firebase from '../../assets/Firebase';
+import HTML from '../../assets/HTML';
+import JavaScript from '../../assets/JavaScript';
+import Jest from '../../assets/Jest';
+import NodeJS from '../../assets/NodeJS';
+import React from '../../assets/React';
+import Redux from '../../assets/Redux';
+import Sass from '../../assets/Sass';
+import TypeScript from '../../assets/TypeScript';
+import VSCode from '../../assets/VSCode';
+import Webpack from '../../assets/Webpack';
 import './About.scss';
 
 function About() {
+  const technicalSkills = [
+    { icon: <React />, name: 'React' },
+    { icon: <Redux />, name: 'Redux' },
+    { icon: <TypeScript />, name: 'TypeScript' },
+    { icon: <JavaScript />, name: 'JavaScript' },
+    { icon: <CSS />, name: 'CSS' },
+    { icon: <Sass />, name: 'SASS' },
+    { icon: <HTML />, name: 'HTML' },
+    { icon: <Firebase />, name: 'Firebase' },
+    { icon: <NodeJS />, name: 'NodeJS' },
+    { icon: <Jest />, name: 'Jest' },
+    { icon: <Babel />, name: 'Babel' },
+    { icon: <Webpack />, name: 'Webpack' },
+    { icon: <ESLint />, name: 'ESLint' },
+    { icon: <VSCode />, name: 'VSCode' },
+  ];
+
   return (
-    <section className='about' id='about'>
-      <div className='column centered_grid'>
+    <section className='about centered_grid full_height' id='about'>
+      <div className='column'>
         <div className='about__content'>
-          <h2>About Me</h2>
-          <p>
-            Hi! My name is Bryan and I love building things both on and offline. My interest in
-            programming began in elementary school: kicking America Online users offline with Visual
-            Basic. In high school, Adobe Photoshop, HTML & CSS caught my interest. Before long, I
-            was responsible for 5 local band web sites and eventually took on a few small paying
-            clients.
-          </p>
+          <div className='about__skills left'>
+            {technicalSkills.slice(0, technicalSkills.length / 2).map((skill) => {
+              return (
+                <div className='about__skill' key={skill.name}>
+                  {skill.icon}
+                  <p>{skill.name}</p>
+                </div>
+              );
+            })}
+          </div>
 
-          <p>
-            Over the years, I have gained experience with BASH, AutoHotkey and PowerShell scripting
-            to automate both personal and work related activities.
-          </p>
+          <div className='about__text'>
+            <div className='about__text-wrapper'>
+              <h2>About Me</h2>
+              <p>
+                Hi! My name is Bryan and I love building things both on and offline. My interest in
+                programming began in elementary school: kicking America Online users offline with
+                Visual Basic. In high school, Adobe Photoshop, HTML & CSS caught my interest. Before
+                long, I was responsible for 5 local band web sites and eventually took on a few
+                small paying clients.
+              </p>
 
-          <p>
-            <a href='https://github.com/bmilcs/dotfiles-old'>Linux</a>, networking & servers became
-            a part of my life in the late 2010s. In my leisure time, I manage a Dell PowerEdge
-            R730XD server, running VMWare ESXI and it hosts a virtualized TrueNAS environment,
-            docker-compose stack, family media server and more. I have dabbled with reverse proxies
-            (NGiNX), the LAMP stack, WordPress and several Linux distros: Debian, Ubuntu, Manjaro,
-            Arch, DietPi, Raspbian, etc.
-          </p>
+              <p>
+                Over the years, I have gained experience with BASH, AutoHotkey and PowerShell
+                scripting to automate both personal and work related activities.
+              </p>
 
-          <p>
-            In mid-2022, I narrowed my focus on becoming a professional developer. Following the{' '}
-            <a href='https://www.theodinproject.com/' target='_blank' rel='noreferrer'>
-              Odin Project
-            </a>
-            , all of my free time (after work and weekends) has gone toward learning the
-            fundamentals of modern web development. Technology and problem solving have been
-            lifelong passions of mine and I am excited to share my experience and continue learning
-            in a professional setting.
-          </p>
+              <p>
+                <a href='https://github.com/bmilcs/dotfiles-old'>Linux</a>, networking & servers
+                became a part of my life in the late 2010s. In my leisure time, I manage a Dell
+                PowerEdge R730XD server, running VMWare ESXI and it hosts a virtualized TrueNAS
+                environment, docker-compose stack, family media server and more. I have dabbled with
+                reverse proxies (NGiNX), the LAMP stack, WordPress and several Linux distros:
+                Debian, Ubuntu, Manjaro, Arch, DietPi, Raspbian, etc.
+              </p>
+
+              <p>
+                In mid-2022, I narrowed my focus on becoming a professional developer. Following the{' '}
+                <a href='https://www.theodinproject.com/' target='_blank' rel='noreferrer'>
+                  Odin Project
+                </a>
+                , all of my free time (after work and weekends) has gone toward learning the
+                fundamentals of modern web development. Technology and problem solving have been
+                lifelong passions of mine and I am excited to share my experience and continue
+                learning in a professional setting.
+              </p>
+            </div>
+          </div>
+
+          <div className='about__skills right'>
+            {technicalSkills.slice(technicalSkills.length / 2).map((skill) => {
+              return (
+                <div className='about__skill' key={skill.name}>
+                  {skill.icon}
+                  <p>{skill.name}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
