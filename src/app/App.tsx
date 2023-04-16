ac073c7dea53cb69448d2c92b37849abda93626d
Bryan Miller <bmilcs@yahoo.com>
2023-04-16 15:25:18 -0400

    Add minor style tweaks & footer to page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import About from '../components/about/About';
 import Contact from '../components/contact/Contact';
+import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 import Hero from '../components/hero/Hero';
 import Portfolio from '../components/portfolio/Portfolio';
@@ -13,6 +14,7 @@ function App() {
       <Portfolio />
       <About />
       <Contact />
+      <Footer />
     </div>
   );
 }
