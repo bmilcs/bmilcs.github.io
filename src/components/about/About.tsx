06613af9429b5d10ac2a148b8532c52ba87ead28
Bryan Miller <bmilcs@yahoo.com>
2023-06-25 18:28:29 -0400

    Rewrite about me section & add skills

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,17 +1,21 @@
 import { useEffect, useRef } from 'react';
 import Babel from '../../assets/components/Babel';
+import Bootstrap from '../../assets/components/Bootstrap';
 import CSS from '../../assets/components/CSS';
 import ESLint from '../../assets/components/ESLint';
+import Express from '../../assets/components/Express';
 import Firebase from '../../assets/components/Firebase';
 import Github from '../../assets/components/Github';
 import HTML from '../../assets/components/HTML';
 import JavaScript from '../../assets/components/JavaScript';
 import Jest from '../../assets/components/Jest';
+import MongoDB from '../../assets/components/MongoDB';
 import NPM from '../../assets/components/NPM';
 import NodeJS from '../../assets/components/NodeJS';
 import React from '../../assets/components/React';
 import Redux from '../../assets/components/Redux';
 import Sass from '../../assets/components/Sass';
+import Tailwind from '../../assets/components/TailwindCSS';
 import TypeScript from '../../assets/components/TypeScript';
 import VIM from '../../assets/components/VIM';
 import VSCode from '../../assets/components/VSCode';
@@ -35,6 +39,9 @@ function About() {
     { icon: <React />, name: 'React' },
     { icon: <Redux />, name: 'Redux' },
     { icon: <TypeScript />, name: 'TypeScript' },
+    { icon: <Express />, name: 'Express' },
+    { icon: <MongoDB />, name: 'MongoDB' },
+    { icon: <NodeJS />, name: 'NodeJS' },
     { icon: <JavaScript />, name: 'JavaScript' },
     { icon: <CSS />, name: 'CSS' },
     { icon: <Sass />, name: 'SASS' },
@@ -45,10 +52,11 @@ function About() {
     { icon: <Webpack />, name: 'Webpack' },
     { icon: <Babel />, name: 'Babel' },
     { icon: <ESLint />, name: 'ESLint' },
-    { icon: <NodeJS />, name: 'NodeJS' },
     { icon: <Firebase />, name: 'Firebase' },
     { icon: <VIM />, name: 'VIM' },
     { icon: <VSCode />, name: 'VSCode' },
+    { icon: <Tailwind />, name: 'TailwindCSS' },
+    { icon: <Bootstrap />, name: 'Bootstrap' },
   ];
 
   return (
@@ -58,19 +66,49 @@ function About() {
           <div className='about__text' ref={leftColumnRef}>
             <h2 id='about__title'>About Me</h2>
             <p>
-              Hi there, I&apos;m Bryan – a <strong>driven web developer</strong> with a passion for
-              programming and knack for problem-solving. With experience building websites since the
-              early 2000&apos;s and managing homelab servers running VMWare,{' '}
-              <a href='https://github.com/bmilcs/dotfiles-old'>Linux</a> virtual machines and Docker
-              containers, I&apos;ve honed my ability to learn and adopt new technology. As I pursue
-              my dream of becoming a professional developer, I&apos;ve been devoting all my free
-              time to learning modern web development through the Odin Project (
+              Hey there, I&apos;m Bryan and I like to build things. My web development journey began
+              as a freshman in high school, and I&apos;ve been hooked on programming ever since.
+            </p>
+
+            <p>
+              My true passions in life are learning and taking on challenges outside of my comfort
+              zone. Over the years, I&apos;ve built computers and mechanical keyboards. I&apos;ve
+              modified cell phones and video game consoles . I&apos;ve spun up countless Linux
+              virtual machines on my homelab server, tinkered with Raspberry Pis, wired my house
+              with Cat6A ethernet cable and deployed enterprise level networking equipment. To top
+              it off, I&apos;ve used{' '}
+              <a href='https://github.com/bmilcs/dotfiles-old' target='_blank' rel='noreferrer'>
+                ArchLinux
+              </a>{' '}
+              as my primary operating system for 6 months, obsessing over every detail of my
+              workstation.
+            </p>
+
+            <p>
+              As time went on, I found myself drawn to writing BASH scripts to automate tasks on my
+              Linux workstation and AutoHotkey scripts to improve my performance in my day job. I
+              realized my true love of coding and knew it&apos;s what I want to do for the rest of
+              my life.
+            </p>
+
+            <p>
+              In the summer of 2022, I set a goal to become a professional web developer. Since
+              then, I have devoted <em>every free minute of my life</em> to the pursuit of my dream.
+              With The Odin Project (
               <a href='https://www.github.com/bmilcs/odin-project' target='_blank' rel='noreferrer'>
-                view my progress
+                view my progress here
               </a>
-              ). I&apos;m excited to put my expertise to work and{' '}
-              <strong>help your business</strong> grow and thrive. Let&apos;s work together to
-              create something amazing!
+              ) as my guide, I have been relentless in my pursuit. Visual Studio Code has become my
+              second home and my{' '}
+              <a href='https://github.com/bmilcs' target='_blank' rel='noreferrer'>
+                GitHub profile
+              </a>{' '}
+              back me up on that.
+            </p>
+            <p>
+              I <strong>cannot wait</strong> to apply my problem solving skills and love of learning
+              toward the completion of your business objectives. I am ready to hit the ground
+              running so let&apos;s get started!
             </p>
           </div>
 
