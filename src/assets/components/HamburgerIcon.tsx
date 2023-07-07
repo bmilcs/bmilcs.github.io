9c4529a97b6f779f24587aa3e6c2b9f378367e42
Bryan Miller <bmilcs@yahoo.com>
2023-07-07 17:12:50 -0400

    Add mobile hamburger menu

---
+++ src/assets/components/HamburgerIcon.tsx
@@ -0,0 +1,16 @@
+function HamburgerIcon() {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+      <path
+        fill='none'
+        stroke='hsla(0, 0%, 97%, 1)'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2'
+        d='M5 17h14M5 12h14M5 7h14'
+      />
+    </svg>
+  );
+}
+
+export default HamburgerIcon;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,23 +1,53 @@
+import { useEffect, useRef, useState } from 'react';
 import BMIcon from '../../assets/components/BMIcon';
+import HamburgerIcon from '../../assets/components/HamburgerIcon';
+import useElementOffScreen from '../../hooks/useElementOffScreen';
 import './Header.scss';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const hamburgerRef = useRef<HTMLButtonElement>(null);
+  const headerRef = useRef<HTMLElement>(null);
+  const isHeaderOffScreen = useElementOffScreen(headerRef);
+
+  useEffect(() => {
+    if (!isHeaderOffScreen) {
+      hamburgerRef.current?.classList.remove('hamburger-menu__sticky');
+      return;
+    }
+    hamburgerRef.current?.classList.add('hamburger-menu__sticky');
+  }, [isHeaderOffScreen]);
+
   return (
-    <header className='column' aria-label='header with navigation'>
+    <header className='column' aria-label='header with navigation' ref={headerRef}>
       <BMIcon />
-      <nav>
+      <nav className={isMenuOpen ? 'nav__open' : 'nav__closed'}>
         <ul>
           <li>
-            <a href='#portfolio'>portfolio</a>
+            <a href='#portfolio' onClick={() => setIsMenuOpen(false)}>
+              portfolio
+            </a>
           </li>
           <li>
-            <a href='#about'>about</a>
+            <a href='#about' onClick={() => setIsMenuOpen(false)}>
+              about
+            </a>
           </li>
           <li>
-            <a href='#contact'>contact</a>
+            <a href='#contact' onClick={() => setIsMenuOpen(false)}>
+              contact
+            </a>
           </li>
         </ul>
       </nav>
+
+      <button
+        className='hamburger-menu'
+        onClick={() => setIsMenuOpen((prev) => !prev)}
+        ref={hamburgerRef}
+      >
+        <HamburgerIcon />
+      </button>
     </header>
   );
 }
